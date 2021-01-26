93f118221788e80f36d8f6b59f6ba2fc7bb5e14a
Timothy Smith <timothytsmith8@gmail.com>
2021-01-26 01:39:41 -0800

    populated three burger rows into burger table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,6 @@
+-- Will insert rows into burger table.
+INSERT INTO burger( burger_name, devoured)
+VALUES("CheddarBurger", false),
+("BLTBurger", false),
+("RedWoodOrigional", false );
 
-
-   * In the `orm.js` file, create the methods that will execute the necessary MySQL 
-   commands in the controllers. These are the methods you will need to use in order
-    to retrieve and store data in your database.
-
-     * `selectAll()`
-     * `insertOne()`
-     * `updateOne()`
-
-   * Export the ORM object in `module.exports`.
