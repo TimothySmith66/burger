af0307effcf342626d2ca01b642eb5e69755a82c
Timothy Smith <timothytsmith8@gmail.com>
2021-01-26 19:36:13 -0800

    express connection set up

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+// express dependency
 const express = require('express');
+// calls express 
+ const app = express();
+
+ const PORT = process.env.PORT || 3001;
+
+// For: data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+
+
+app.get("/notes", (req, res) =>{
+res.sendFile('public/notes.html')
+
+});
+
+
+
+//port listener
+app.listen(PORT, () => {
+    console.log("App listening on PORT " + PORT);
+});
